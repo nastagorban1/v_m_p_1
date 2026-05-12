9ca548669baadcc04e64326f9da96ccdef037dd1
Nastya Gorban <nastagorban02@gmail.com>
2026-05-12 23:23:19 +0300

    add User entity, UsersModule, migration

---
+++ src/users/user.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+import { Role } from '../common/enums/role.enum';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  passwordHash!: string;
+
+  @Column({ nullable: true })
+  name!: string;
+
+  @Column({ type: 'enum', enum: Role, default: Role.USER })
+  role!: Role;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
